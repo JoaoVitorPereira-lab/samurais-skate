337cc1f3709986e517fd58602ad681f627595682
JoaoVitorPereira-lab <v17112005@gmail.com>
2022-09-03 19:08:22 -0300

    commit 2

--- site/src/pages/components/rodape/index.js
+++ site/src/pages/components/rodape/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 export default function Rodape(){
     return(
         <section className='rodape-container'>
+            <div className="div-separa-container">
             <div>
                 <img src="./images/logo.png" alt="" className="logo-rodape"/>
             </div>
@@ -11,11 +12,11 @@ export default function Rodape(){
                 <h2 className='h2-contatos'> Contatos </h2>
 
                 <div className="contato email">
-                    <img src="./images/email.png" alt="" className="logo-contatos"/>
+                    <img src="./images/email.png" alt=""/>
                     <span> skatesup@gmail.com </span>
                 </div>
                 <div className="contato telefone">
-                    <img src="./images/telefone.png" alt="" className="logo-contatos"/>
+                    <img src="./images/telefone.png" alt=""/>
                     <span className="span-telefone"> (11) 94002-8922 </span>
                 </div>
             </div>
@@ -24,38 +25,39 @@ export default function Rodape(){
                 <h2 className='h2-rdsc'> Redes Sociais </h2>
 
                 <div className="rdsc">
-                    <img src="./images/instagram.png" alt=""/>
-                    <img src="./images/facebook.png" alt=""/>
+                    <img src="./images/instagram.png" alt="" className="instagram-rodape"/>
+                    <img src="./images/facebook.png"  alt="" className="facebook-rodape"/>
                 </div>
             </div>
 
             <div className='div-forma-pagamento'>
-                <h2 className='h2-forma-pagamento'> Forma de Pagamento </h2>
+                <h2 className='h2-forma-pagamento'> Formas de Pagamento </h2>
 
                 <div className="pag-a-vista">
-                    <h2 className='h2-pag-a-vista'> Pagamento à Vista </h2>
+                    <h3 className='h3-pag-a-vista'> Pagamento à Vista </h3>
 
                     <div className="icon-pag-a-vista">
-                        <img src="./images/boleto.png" alt=""/>
-                        <img src="./images/pix-rodape.png" alt=""/>
-                        <img src="./images/deposito.png" alt=""/>
+                        <img src="./images/boleto.png"      alt="" className="boleto"/>
+                        <img src="./images/pix-rodape.png"  alt="" className="pix-rodape"/>
+                        <img src="./images/deposito.png"    alt="" className="deposito"/>
                     </div>
                 </div>
 
                 <div className="pag-a-prazo">
-                    <h2 className='h2-pag-a-prazo'> Pagamento à Prazo </h2>
+                    <h3 className='h3-pag-a-prazo'> Pagamento à Prazo </h3>
 
                     <div className="icon-pag-a-prazo">
-                        <img src="./images/mastercard.png" alt=""/>
-                        <img src="./images/american-express.png" alt=""/>
-                        <img src="./images/visa.png" alt=""/>
-                        <img src="./images/elo.png" alt=""/>
-                        <img src="./images/hiper.png" alt=""/>
-                        <img src="./images/hipercard.png" alt=""/>
-                        <img src="./images/diners-club.png" alt=""/>
-                        <img src="./images/mercado-pago.png" alt=""/>
+                        <img src="./images/mastercard.png"       alt="" className="icon mastercard"/>
+                        <img src="./images/american-express.png" alt="" className="icon2 american-express"/>
+                        <img src="./images/visa.png"             alt="" className="icon visa"/>
+                        <img src="./images/elo.png"              alt="" className="icon elo"/>
+                        <img src="./images/hiper.png"            alt="" className="icon hiper"/>
+                        <img src="./images/hipercard.png"        alt="" className="icon2 hipercard"/>
+                        <img src="./images/diners-club.png"      alt="" className="icon diners-club"/>
+                        <img src="./images/mercado-pago.png"     alt="" className="icon mercado-pago"/>
                     </div>
                 </div>
+            </div>
             </div>
         </section>
     )
