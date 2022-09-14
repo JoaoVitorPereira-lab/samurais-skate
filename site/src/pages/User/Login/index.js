8d504508f8b60fec158e6923aea0a23c55e0d377
João Pedro Nunes Cunha <joaopedronunescunha7@gmail.com>
2022-09-14 18:18:18 -0300

    Login

---
+++ site/src/pages/User/Login/index.js
@@ -0,0 +1,15 @@
+import "./index.scss"
+
+
+export default function Login() {
+    return (
+        <section>
+            <div>
+            <img src="../images/logo.png" alt="logo" className="logo"/>
+            </div>
+            <div className="barra_lateral">
+
+            </div>
+        </section>
+    )
+}
