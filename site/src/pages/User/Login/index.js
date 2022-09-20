45e5f97ac26c026df93e625543b4ec1cd69d7f0a
João Vitor <v17112005@gmail.com>
2022-09-20 18:17:27 -0300

    Login 70% feito

--- site/src/pages/User/Login/index.js
+++ site/src/pages/User/Login/index.js
@@ -3,11 +3,24 @@ import "./index.scss"
 
 export default function Login() {
     return (
-        <section>
+        <section className="page-login">
             <div>
-            <img src="../images/logo.png" alt="logo" className="logo"/>
+                <img src="../images/logo.png" alt="logo" className="logo" />
             </div>
-            <div className="barra_lateral">
+            <div className="barra_lateral"></div>
+            <div className="botoes">
+                <h1>Entrar</h1>
+                <div className="cx1">
+                    <h6>E-mail</h6>
+                    <input className="email" type="text"></input>
+                </div>
+                <p></p>
+                <div className="cx2">
+                    <h6>Senha</h6>
+                    <input className="senha" type="text"></input>
+                    <button className="esqueceu">Esqueceu a senha?</button>
+                    <button className="entrar">Entrar</button>
+                </div>
 
             </div>
         </section>
