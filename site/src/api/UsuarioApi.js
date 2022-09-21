6433d7d2983e7864c5cdef4ade4619cf8a4815a4
Pedro Henrique Rodrigues Reis <pedroreisrodrigues2905@gmail.com>
2022-09-21 16:32:08 -0300

    Alterações

--- site/src/api/UsuarioApi.js
+++ site/src/api/UsuarioApi.js
@@ -2,3 +2,12 @@ import axios from "axios";
 const api = axios.create({
     baseURL:'http://localhost:5000'
 })
+
+
+export default function Login(email,senha){
+    const resposta = await api.post('/api/login', {
+        email: email,
+        senha: senha
+    })
+    return resposta.data
+}
