f990d90111587ff8014d05314838d461ce0f309c
Pedro Henrique Rodrigues Reis <pedroreisrodrigues2905@gmail.com>
2022-10-04 18:18:44 -0300

    Para o yag do rodrigo

---
+++ site/src/pages/components/cabecalhoUser/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./index.scss";
+
+export default function App() {
+  return (
+    
+    <main>
+
+        <div className="div-logo">
+
+            <div>
+            <img src="../images/logo.png" width="124.8px" />
+            </div>
+
+            <div className="infos">
+
+            </div>
+
+        </div>
+
+    <nav className="navs-container">
+        
+
+        <div className="div-navs">
+            <div className="navs-produtos not">
+                <img src="../images/skate.png" className="imgs-nav" alt=""/>
+                <span> Skate </span>
+            </div>
+
+            <div className="navs-produtos">
+                <img src="../images/tenis.png" className="imgs-nav" alt=""/>
+                <span> Tênis </span>
+            </div>
+
+            <div className="navs-produtos">
+                <img src="../images/bone.png" className="imgs-nav" alt=""/>
+                <span> Boné </span>
+            </div>
+
+            <div className="navs-produtos">
+                <img src="../images/acessorios.png" className="imgs-nav" alt=""/>
+                <span> Acessórios </span>
+            </div>
+
+            <div className="navs-produtos">
+                <img src="../images/promocoes.png" className="imgs-nav" alt=""/>
+                <span> Promoções </span>
+            </div>
+        </div>
+
+        <div className="div-buscar">
+            <input type="text" className="buscar-input" placeholder="Buscar"/>
+            <div>
+                <img src="../images/buscar.png" className="buscar-img" alt=""/>
+            </div>
+        </div>
+
+    </nav></main>
+
+  );
+}
