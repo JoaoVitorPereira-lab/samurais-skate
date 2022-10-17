b3e32bfc8134da1186772205194932d548bca40a
João Pedro <joaopedronunescunha7@gmail.com>
2022-10-16 22:14:33 -0300

    tentando fazer o carrossel

--- site/src/pages/Landing_Page/index.js
+++ site/src/pages/Landing_Page/index.js
@@ -4,7 +4,16 @@ import { useNavigate } from 'react-router-dom'
 import LoadingBar from 'react-top-loading-bar'
 import { toast } from 'react-toastify'
 
-import Cabecalho from '../components/cabecalhoUser';
+import Cabecalho from '../components/cabecalhoUser'
+
+
+//* IMAGENS PARA O CARROSSEL*//
+
+import images1 from './images/1.jpg'
+import images2 from './images/2.jpg'
+import images3 from './images/3.jpg'
+
+
 
 
 
@@ -20,14 +29,54 @@ export default function Landing_Page() {
         <main className='landing-page'>
 
             <div className='componentes'>
-                <Cabecalho/>   
+                <Cabecalho />
             </div>
 
             <section className='carrosel'>
-                <div className='fotos'>
-                   <img src="./images/1" alt="" />
-                
+
+
+                <div className='slides'>
+
+                    <input className='radio-btn' type="radio" id='radio1' />
+                    <input className='radio-btn' type="radio" id='radio2' />
+                    <input className='radio-btn' type="radio" id='radio3' />
+
+
+                    <div className='foto1'>
+                        <img className='image1' src={images1} />
+                    </div>
+
+                    <div className='foto2'>
+                        <img className='image2' src={images2} />
+                    </div>
+
+                    <div className='foto3'>
+                        <img className='image3' src={images3} />
+                    </div>
+
+
+                    <div className='nav-auto'>
+
+                        <div className='auto-btn1'></div>
+                        <div className='auto-btn2'></div>
+                        <div className='auto-btn3'></div>
+
+                    </div>
+
+                    <div className='nav-manual'>
+
+                        <label for='radio1' className='manual-btn1'></label>
+                        <label for='radio2' className='manual-btn2'></label>
+                        <label for='radio3' className='manual-btn3'></label>
+
+                    </div>
+
+
+
                 </div>
+
+
+
             </section>
         </main>
     );
