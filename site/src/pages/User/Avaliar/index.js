df5c718bebc26ae6ffc4f0dbe5415ce45edc735d
Rodrigo <rodrigo.anjos0806@gmail.com>
2022-11-18 06:40:09 -0300

    deus nos ajude

--- site/src/pages/User/Avaliar/index.js
+++ site/src/pages/User/Avaliar/index.js
@@ -1,9 +1,21 @@
 import Cabecalho from '../../components/cabecalho'
+import './index.scss'
 
 export default function Avaliar() {
     return(
-        <main>
+        <main className='aval-container'>
             <Cabecalho />
+            <section className='info'>
+                <div className='titulo'>
+                    <hr />
+                    <h2> Avalie o produto: <span className="span-status">a  </span> </h2>
+                </div>
+                <div className='inputs'>
+                    <h2>Descreva sua avaliação: </h2>
+                    <textarea placeholder='Avaliação' cols="50" rows="10"></textarea>
+                </div>
+                
+            </section>
         </main>
     );
 }
